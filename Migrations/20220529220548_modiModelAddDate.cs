eaee42b9bdd3f2216b9284bc58cb7deb71d39c06
tndevfactory <tndevfactory@gmail.com>
2022-05-29 23:07:45 +0100

    datetime

---
+++ Migrations/20220529220548_modiModelAddDate.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tracerapi.Migrations
+{
+    public partial class modiModelAddDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateProchaineEcheance",
+                table: "Taches",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateEcheance",
+                table: "Taches",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateContrat",
+                table: "Taches",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAppel",
+                table: "Taches",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateProchaineEcheance",
+                table: "Interventions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateEcheance",
+                table: "Interventions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateContrat",
+                table: "Interventions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAppel",
+                table: "Interventions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateProchaineEcheance",
+                table: "Taches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateEcheance",
+                table: "Taches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateContrat",
+                table: "Taches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateAppel",
+                table: "Taches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateProchaineEcheance",
+                table: "Interventions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateEcheance",
+                table: "Interventions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateContrat",
+                table: "Interventions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateAppel",
+                table: "Interventions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Tache.cs
+++ Models/Tache.cs
@@ -5,12 +5,12 @@
         public int Id { get; set; }
         public string Sujet { get; set; } = string.Empty;
         public string Compte { get; set; } = string.Empty;
-        public string DateEcheance { get; set; } = string.Empty;
-        public string DateProchaineEcheance { get; set; } = string.Empty;
-        public string DateContrat { get; set; } = string.Empty;
+        public DateTime? DateEcheance { get; set; } 
+        public DateTime? DateProchaineEcheance { get; set; } 
+        public DateTime? DateContrat { get; set; } 
         public string CodeContrat { get; set; } = string.Empty;
         public string ContratMaintenance { get; set; } = string.Empty;
-        public string DateAppel { get; set; } = string.Empty;
+        public DateTime? DateAppel { get; set; } 
         public string Type { get; set; } = string.Empty;
         public string Note { get; set; } = string.Empty;
         public string Owner { get; set; } = string.Empty;
